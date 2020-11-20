9da022a65327e5e4805f9f58ca3e0e9b92b83b83
adradan <ericvsolcruz@gmail.com>
2020-11-19 22:38:19 -0500

    finished version, pre-testing

--- muter/bot.py
+++ muter/bot.py
@@ -1,17 +1,55 @@
 import discord
 import re
-from discord.ext import commands
+from discord.ext import commands, tasks
 from .config import Config
+import pymongo
+from pymongo import MongoClient
+import urllib
+import datetime
+import asyncio
+# import time
 from .checks.messages import is_sent_by_bot
 
 
 class Muter(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
+    def __init__(self, discord_bot: commands.Bot,
+                 mongo_client: MongoClient,
+                 mute_db,
+                 mute_collection):
+        self.client = mongo_client
+        self.db = mute_db
+        self.mutes = mute_collection
+        self.bot = discord_bot
+        self.guild = None
+        self.prev_time = datetime.datetime.now().replace(microsecond=0)
+        self.read_database.start()
+
+    def cog_unload(self):
+        self.read_database.cancel()
+        self.mutes.delete_many({'user_id': 473350400054001666})
+
+    @tasks.loop(seconds=1)
+    async def read_database(self):
+        channel = await self.bot.fetch_channel('777013090969583616')
+        new_time = datetime.datetime.now().replace(microsecond=0)
+        if new_time.second != self.prev_time.second:
+            self.prev_time = new_time
+            results = self.mutes.find({'end_time': str(new_time)})
+            for user in results:
+                guild = self.bot.get_guild(self.guild)
+                member = guild.get_member(user_id=user['user_id'])
+                role = discord.utils.get(member.guild.roles, name="Muted")
+                await member.remove_roles(role)
+                await channel.send(f'Time limit reached! Unmuting <@!{user["user_id"]}>...')
+
+    @read_database.before_loop
+    async def before_read_database(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.id == bot.user.id:
+        if not message.author.id == self.bot.user.id:
             print('Not the Bot')
 
     @commands.command(name='prefix', brief='Changes the prefix of Self-Muter')
@@ -37,13 +75,20 @@ class Muter(commands.Cog):
         args : str
             Takes in user-input. Will check for time notations (hours, minutes, etc.)
         """
+        self.guild = ctx.guild.id
         time = await self.check_time_arg(*args)
+        member = ctx.message.author
         if not time:
-            await ctx.send('```Syntax: (digit)(unit; up to 8 hours; h, m, s)\nExample: 10m (10 minutes)'
+            await ctx.send('```Syntax: (digit)(unit; 5 secs to 8 hrs; h, m, s)\nExample: 10m (10 minutes)'
                            + f'\nGiven Arguments: {" ".join(args)}```')
             return
+        end = await self.calculate_end_time(time[0])
+        await self.add_user_to_db(ctx.message.author.id, end)
         length = time[1]
         length = f'{length.get("h", 0)}h {length.get("m", 0)}m {length.get("s", 0)}s'
+        role = discord.utils.get(member.guild.roles, name="Muted")
+        print(member)
+        await member.add_roles(role)
         await ctx.send(f'Muting {ctx.message.author.mention} for {length}...')
         return
 
@@ -51,17 +96,17 @@ class Muter(commands.Cog):
         """Checks for time notation (hours, minutes, etc)"""
         mute_times = {}
         acceptable_units = ['h', 'm', 's']
-        for time in args:
+        for times in args:
             digit = re.compile(r'([\d]+)([a-zA-Z]+)')
             try:
-                digit_match = digit.match(time).groups()
+                digit_match = digit.match(times).groups()
             except AttributeError:
                 return {}
             unit = digit_match[1]
-            time = digit_match[0]
+            times = digit_match[0]
             if mute_times.get(unit) or unit not in acceptable_units:
                 return {}
-            mute_times[unit] = time
+            mute_times[unit] = times
         total = await self.validate_times(mute_times)
         if not total:
             return {}
@@ -83,19 +128,37 @@ class Muter(commands.Cog):
         minutes = int(duration.get('m', 0))
         seconds = int(duration.get('s', 0))
         total += (hours * 3600) + (minutes * 60) + seconds
-        print(total)
-        if total > 28800:
+        if total > 28800 or total < 5:
             return False
         return total, duration
 
+    async def calculate_end_time(self, seconds):
+        now = datetime.datetime.now().replace(microsecond=0)
+        end = now + datetime.timedelta(seconds=seconds)
+        return end
 
-bot = commands.Bot(command_prefix='?', case_insensitive=True, description='Self Muter')
-bot.add_cog(Muter(bot))
+    async def add_user_to_db(self, user, end_time):
+        end_time = end_time.replace(microsecond=0)
+        user = {
+            'user_id': user,
+            'end_time': str(end_time)
+        }
+        mute = self.mutes.insert_one(user)
+
+
+client = MongoClient(Config.MONGO_URL)
+db = client.automuter
+mutes = db.selfmuter
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='?', case_insensitive=True, description='Self Muter', intents=intents)
+bot.add_cog(Muter(bot, client, db, mutes))
 
 
 @bot.event
 async def on_ready():
     print(f'Logged in as: {bot.user}.')
+    loop = asyncio.get_event_loop()
 
 
 bot.run(Config.TOKEN)
--- muter/config.py
+++ muter/config.py
@@ -1,5 +1,9 @@
 import os
+import urllib
 
 
 class Config:
     TOKEN = os.getenv('MUTER_TOKEN')
+    user = urllib.parse.quote_plus(os.getenv('MONGO_USER'))
+    pw = urllib.parse.quote_plus(os.getenv('MONGO_PW'))
+    MONGO_URL = f'mongodb+srv://{user}:{pw}@automuter.7ypk1.mongodb.net/automuter.selfmuter?retryWrites=true&w=majority'
